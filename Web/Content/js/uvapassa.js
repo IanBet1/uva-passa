651491dc3e742efc822f2de48c7ea93f2a566998
wilsonfaria86 <wilsonfaria86@gmail.com>
2018-01-24 16:09:30 -0200

    Paginação

--- Web/Content/js/uvapassa.js
+++ Web/Content/js/uvapassa.js
@@ -6,13 +6,15 @@ $(document).ready(function(){
 	});
 });
 
-function Pesquisar(){
+function Pesquisar(selectedPage){
 	//alert('UvaPassa');
 	$("#divResultado").html("");
 	
+	if(!selectedPage) selectedPage = 1;
+	
 	var textoPesquisa = $('#txtPesquisa').val();
 	
-	$.getJSON( "https://api.themoviedb.org/3/search/movie?api_key=1feaee1568e8ea65c78670425ea80323&query=" + textoPesquisa, function( data ) {
+	$.getJSON( "https://api.themoviedb.org/3/search/movie?api_key=1feaee1568e8ea65c78670425ea80323&query=" + textoPesquisa + "&page=" + selectedPage, function( data ) {
 	  var items = [];
 	  $.each( data.results, function( key, val ) {
 		var d = new Date(val.release_date);
@@ -20,7 +22,10 @@ function Pesquisar(){
 		var titulo =  val.title;
 		if(val.original_title.toLowerCase().trim() != val.title.toLowerCase().trim())
 			titulo = val.original_title  + " - " + "<i>" + titulo + "</i> ";
-		titulo = titulo + " (" + d.getFullYear() + ")";
+		var anoLancamento = "";
+		if(d && d.toString() != "Invalid Date"){
+			anoLancamento = " (" + d.getFullYear() + ")";
+		}
 		
 		var poster = ""; 
 		if(val.poster_path)
@@ -28,18 +33,44 @@ function Pesquisar(){
 		else
 			poster = "Content/images/noimage.png";
 		
-		var htmlItem = 	"<div id='" + val.id + "' class='table-cell ItemResultado'>" + 
+		// var htmlItem = 	"<div id='" + val.id + "' class='table-cell ItemResultado'>" + 
+							// "<img src='" + poster + "' class='Imagem' /><br/>" +
+							// "<span class='Titulo'>" + titulo + "</span><br/>" +
+							// "<span class='Titulo'>" + anoLancamento + "</span>" +
+						// "</div>";
+		
+		var htmlItem = 	"<div id='" + val.id + "' class='ItemResultado'>" + 
 							"<img src='" + poster + "' class='Imagem' /><br/>" +
-							"<span class='Titulo'>" + titulo + "</span>" +
+							"<span class='Titulo'>" + titulo + "</span><br/>" +
+							"<span class='Titulo'>" + anoLancamento + "</span>" +
 						"</div>";
-		
-		
 		items.push(htmlItem);
+		
+		if(data.total_pages > 1){
+			$('#ulpaginacao').pagination({
+				items: data.total_pages,
+				itemOnPage: 10,
+				currentPage: selectedPage,
+				cssStyle: 'light-theme',
+				prevText: '<span aria-hidden="true">&laquo;</span>',
+				nextText: '<span aria-hidden="true">&raquo;</span>',
+				onInit: function () {
+					// fire first page loading
+				},
+				onPageClick: function (page, evt) {
+					Pesquisar(page);
+				}
+			});
+		}
+		
 	  });
 	 
-	  $( "<ul/>", {
-		"class": "my-new-list",
-		html: items.join( "" )
-	  }).appendTo( "#divResultado" );
+	  // $( "<ul/>", {
+		// "class": "my-new-list",
+		// html: items.join( "" )
+	  // }).appendTo( "#divResultado" );
+	  
+	  $('#divResultado').html(items.join( "" ));
+	  	  
 	});
 }
